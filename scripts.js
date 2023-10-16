af0dc48f41bb98498a2e2547b7c7276394ae2f13
carloshenrique48 <fariascarlos2346@gmail.com>
2023-10-16 09:22:53 -0300

    conclusão de toda a logica

--- scripts.js
+++ scripts.js
@@ -33,5 +33,23 @@ function convertValues(){
 
 }
 
+function changeCurrency() {
+   const currencyName = document.getElementById("currency-name")
+   const currencyImage = document.querySelector(".currency-img")
 
+   if (currencySelect.value == "dolar") {
+      currencyName.innerHTML = "Dólar americano"
+      currencyImage.src = "./assets/dolar.png"
+   }
+
+   if (currencySelect.value == "euro") {
+      currencyName.innerHTML == "Euro"
+      currencyImage.src = "./assets/euro.png"
+   }
+
+   convertValues()
+
+}
+
+currencySelect.addEventListener("change", changeCurrency)
 convertButton.addEventListener("click", convertValues)
